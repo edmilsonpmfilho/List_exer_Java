959302cefa1003d64a5e42e088bd5ce78e252a94
Edmilson Pessôa de Melo Filho <edmilsonpmfilho@gmail.com>
2022-09-17 16:00:50 -0300

    Adicionando exercicios prontos

---
+++ List_26.java
@@ -0,0 +1,19 @@
+package lista1;
+
+import java.util.Scanner;
+
+public class List_26 {
+    public static void main(String[] args){
+        double A;
+        int I;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Qual a altura: ");
+        A = sc.nextDouble();
+        System.out.println("Qual a idade: ");
+        I = sc.nextInt();
+        if(A>=1.5&&I>=12){
+            
+        }
+        sc.close();
+    }
+}
